878ee5a91681d7d80ac665b4584f12b2e5eb8fc0
031190 <mihail.claudiu.moldovaun@gmail.com>
2025-07-11 10:11:03 +0300

    updates

--- src/main/java/com/emag/pages/BasePage.java
+++ src/main/java/com/emag/pages/BasePage.java
@@ -1,4 +1,49 @@
 package com.emag.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    public final int defaultWaitTimeSeconds = 5;
+    public WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void clickElement(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(defaultWaitTimeSeconds));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+
+    }
+
+    protected void waitForElementToBeVisible(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(defaultWaitTimeSeconds));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected void scrollToElementJs(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) this.driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", new Object[]{element});
+    }
+
+    protected void scrollToElement(WebElement webElement) {
+        Actions actions = new Actions(this.driver);
+        actions.moveToElement(webElement);
+        actions.perform();
+    }
+
+    protected void clickElementJs(WebElement webElement) {
+        JavascriptExecutor executor = (JavascriptExecutor) this.driver;
+        executor.executeScript("arguments[0].click();", new Object[]{webElement});
+    }
 }
